414caa0db49ddabb49b63e15c5d151da54c59f3b
Blacks_G <blacks_and_whites@me.com>
2012-10-31 01:48:35 +0900

    add open/save file system

--- markdown.js
+++ markdown.js
@@ -3,6 +3,8 @@ var markdown = require('markdown');
 var win = gui.Window.get();
 win.resizeTo(800, 600);
 
+var fs = require('fs');
+
 function changeToMarkdown(input){
 	input = input.replace(/<br>/g,"\n");
 	input = input.replace(/<div>/g,"\n");
@@ -16,7 +18,7 @@ function changeToMarkdown(input){
 }
 
 $(window).keydown(function(event){
-        if(event.keyCode == 9) { return false; }  
+	if(event.keyCode == 9) { return false; }  
 });
 
 $("#inputArea").live("focus",function(){
@@ -25,7 +27,7 @@ $("#inputArea").live("focus",function(){
 	return that;
 }).live('blur keyup paste',function(event){
 	var that = $(this);
-	
+
 	if(that.data('before') !== that.html()){
 		that.data('before',that.html());
 		changeToMarkdown(that.html());
@@ -33,7 +35,56 @@ $("#inputArea").live("focus",function(){
 	return that;
 });
 
-$( "#optionsRadiosHTML,#optionsRadiosView" ).live("click", function() { 
-	console.log('clicked!');
+$("#optionsRadiosHTML,#optionsRadiosView").live("click", function() { 
 	changeToMarkdown($("#inputArea").html());
 });
+
+
+$("#fileDialog").live("change",function(){
+	var val = $(this).val();
+	var path = val.replace(/\\/g, '/');
+	if(!path.match(/\.(txt|md|markdown)$/i)){
+		alert('このファイル形式には対応していません');
+		return;
+	}
+	fs.readFile(path, 'utf8',function (err, data) {
+		if (err) throw err;
+		$('#inputArea').html(data.toString());
+		changeToMarkdown($("#inputArea").html());
+	});
+
+});
+
+$("#open").live('click',function(){
+	var chooser = $('#fileDialog');
+	chooser.trigger('click'); 
+});
+
+$("#save").live('click',function(){
+	var chooser = $('#saveDialog');
+	chooser.trigger('click'); 
+});
+
+$('#exit').live('click',function(){
+	win.close();
+});
+
+$("#saveDialog").live("change",function(){
+	var input = $('#inputArea').html();
+	input = input.replace(/<br>/g,"\n");
+	input = input.replace(/<div>/g,"\n");
+	input = input.replace(/<\/div>/g,"");
+
+	var val = $(this).val();
+	var path = val.replace(/\\/g, '/');
+	if(!path.match(/\.(txt|md|markdown)$/i)){
+		alert('このファイル形式には対応していません');
+		return;
+	}
+	fs.writeFile(path, input , function (err) {
+		if (err) throw err;
+		alert('保存が完了しました');
+	});
+
+});
+
